c2d044da2515627151d6bfe0e944e26251b923c3
Chinthani <chinthani96@gmail.com>
2022-04-22 17:03:49 +0530

    Add auth component

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import AllProducts from '../containers/AllProducts';
+import Auth from '../containers/Auth';
 
 const Router  = () => {
     return (
         <BrowserRouter>
-        <Switch>
-            <Route exact path={"/products"} component={AllProducts}/>
-        </Switch>
+            <Switch>
+                <Route exact path={"/products"} component={AllProducts}/>
+                <Route exact path={"/login"} component={Auth}/>
+            </Switch>
         </BrowserRouter>
     )
 }
---
+++ src/containers/Auth.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import { Alert, Button, Container,TextField, Theme, Typography } from '@mui/material';
+import {makeStyles} from '@mui/styles';
+import {PRODUCTS} from '../shared/routes';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles((theme: Theme) => ({
+    paper: {
+        height: "40vh",
+        marginTop:"20vh",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    form: {
+        marginTop:"3vh",
+        padding:"0 2vw 0 2vw"
+    },
+    button:{
+        marginTop:"10px"
+    },
+    typo:{
+        textAlign:"center",
+    },
+    txtInput:{
+        color:"whitesmoke"
+    }
+}))
+
+const Auth = () => {
+    const isSignedUp = true;
+    const history =  useHistory();
+    const [name, setName] = useState("123");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const classes = useStyles();
+
+    const [valid,setValid] = useState(true)
+
+    const handleInputChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        switch(event.target.name){
+            case "username" :
+                setName(event.target.value);
+            case "email" :
+                setEmail(event.target.value);   
+            case "password" :
+                setPassword(event.target.value); 
+        }
+
+        console.log(event.target.value);
+        console.log(name);
+    }
+
+    const loginHandle = () => {
+        const username = "test";
+        const pwd = "test";
+        
+        (name===username && password===pwd)? history.push(PRODUCTS) : setValid(false);
+    
+    }
+    
+    const signUpHandle = () => {
+        history.push(PRODUCTS);
+    }
+
+
+    return(
+        <Container className={classes.paper} maxWidth="sm">
+            {!valid? <Alert severity="error">This is an error alert — check it out!</Alert> : ""}
+            <Typography className={classes.typo} variant="h5" component="h6" >
+                {   isSignedUp? 
+                        "LOGIN" : "SIGNUP" 
+                }
+            </Typography>
+            <form className={classes.form}>
+                <TextField 
+                    margin="normal" 
+                    className={classes.txtInput} 
+                    required 
+                    name="name"
+                    id="username" 
+                    label="username" 
+                    variant="standard" 
+                    fullWidth 
+                    autoFocus
+                    onChange = {(event) => {setName(event.target.value)}}
+                    />
+                {!isSignedUp ? 
+                <TextField 
+                    margin="normal" 
+                    className={classes.txtInput} 
+                    required 
+                    name="email"
+                    id="email" 
+                    label="email" 
+                    variant="standard" 
+                    fullWidth 
+                    autoFocus
+                    onChange = {(event) => {setEmail(event.target.value)}}
+                    /> : <></>}
+                <TextField 
+                    margin="normal" 
+                    className={classes.txtInput} 
+                    required 
+                    name="password"
+                    id="password" 
+                    label="password" 
+                    variant="standard" 
+                    fullWidth 
+                    autoFocus
+                    onChange = {(event) => {setPassword(event.target.value)}}
+                    />
+                {isSignedUp ? 
+                <>
+                <Button 
+                    variant="contained" 
+                    className={classes.button} 
+                    type="submit" 
+                    fullWidth 
+                    color="secondary" 
+                    onClick={loginHandle}
+                    >
+                        Login
+                </Button> 
+                <Typography>Forgot password?</Typography>
+                </>
+                :
+                <Button 
+                    variant="contained" 
+                    className={classes.button} 
+                    type="button" 
+                    fullWidth 
+                    color="secondary" 
+                    onClick={signUpHandle}
+                >Sign up</Button>  
+                }
+            </form>
+        </Container>
+    )
+}
+
+export default Auth;
